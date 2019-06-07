6baddd45cbe87894ad33f9c1e343eb75f7262a20
ljosberinn <admin@gerritalex.de>
2019-06-08 01:20:11 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -28,6 +28,21 @@ const App = () => (
         </div>
       </main>
     </div>
+    <div
+      className="footer container-lg width-full p-responsive"
+      role="contentinfo"
+    >
+      <div className="position-relative d-flex flex-row-reverse flex-lg-row flex-wrap flex-lg-nowrap flex-justify-center flex-lg-justify-between pt-6 pb-2 mt-6 f6 text-gray border-top border-gray-light ">
+        <ul className="list-style-none d-flex flex-wrap col-12 col-lg-5 flex-justify-center flex-lg-justify-between mb-2 mb-lg-0">
+          <li className="mr-3 mr-lg-0">
+            Design © 2019 GitHub, Inc., modifications by myself
+          </li>
+        </ul>
+      </div>
+      <div className="d-flex flex-justify-center pb-6">
+        <span className="f6 text-gray-light" />
+      </div>
+    </div>
   </>
 );
 
