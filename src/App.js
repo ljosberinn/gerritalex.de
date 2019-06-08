c441f202290cb28fdfa884b588ef8466092eb42e
ljosberinn <admin@gerritalex.de>
2019-06-08 19:15:41 +0200

    adding indirect cronjob

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import './assets/App.css';
+import './assets/App.scss';
 
 import { Header } from './components/Header';
+import { Banner } from './components/Banner';
 import { Person } from './components/Person';
 import { DynamicContent } from './components/DynamicContent';
 import { DynamicSubNav } from './components/DynamicSubNav';
@@ -10,13 +11,25 @@ import { CurriculumVitae } from './components/CurriculumVitae';
 const ctaText = 'applying as JavaScript Developer';
 const name = 'Gerrit Alex';
 const userName = 'ljosberinn';
+const repoLink = `https://github.com/${userName}/gerritalex.de`;
+
+(async () => {
+  const response = await fetch('https://dev.gerritalex.de/updater.php');
+  const text = await response.text();
+
+  if (text === 'Update successful.') {
+    // eslint-disable-next-line
+    location.reload();
+  }
+})();
 
 const App = () => (
   <>
-    <Header userName={userName} name={name} />
+    <Header repoLink={repoLink} name={name} />
     <div className="application-main">
       <main>
         <div className="container-xl clearfix px-3 mt-4">
+          <Banner repoLink={repoLink} />
           <Person name={name} userName={userName} ctaText={ctaText} />
           <div className="col-lg-9 col-md-8 col-12 float-md-left pl-md-2">
             <DynamicSubNav userName={userName} />
