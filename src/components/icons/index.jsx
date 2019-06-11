5397c945739d314c2ab56a9a8b46db4f32a7ef28
ljosberinn <admin@gerritalex.de>
2019-06-11 15:23:58 +0200

    extracted several components

--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -2,5 +2,14 @@ import { OcticonBook } from './OcticonBook';
 import { OcticonRepo } from './OcticonRepo';
 import { OcticonCode } from './OcticonCode';
 import { OcticonStar } from './OcticonStar';
+import { OcticonLocation } from './OcticonLocation';
+import { OcticonLink } from './OcticonLink';
 
-export { OcticonBook, OcticonRepo, OcticonCode, OcticonStar };
+export {
+  OcticonBook,
+  OcticonRepo,
+  OcticonCode,
+  OcticonStar,
+  OcticonLocation,
+  OcticonLink
+};
