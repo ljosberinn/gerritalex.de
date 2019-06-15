8e244ded14012e5c65eee961f70e5a57b6199fff
ljosberinn <admin@gerritalex.de>
2019-06-15 16:57:47 +0200

    added dialog polyfill (hi firefox)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './i18n';
+import dialogPolyfill from 'dialog-polyfill';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (!('HTMLDialogElement' in window)) {
+  dialogPolyfill.registerDialog(document.querySelector('dialog'));
+}
