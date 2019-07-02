92fc8c29e57d112035312cf3fa5b33f8b3c8f691
ljosberinn <admin@gerritalex.de>
2019-07-02 22:42:10 +0200

    updated biography

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+const currentBiographyParagraphs = 5;
+
 export const Biography = () => {
   const { t } = useTranslation();
 
-  return <div className="p-note user-profile-bio">{t('biography')}</div>;
+  return (
+    <div className="p-note user-profile-bio">
+      <ul className="vcard-details mb-3">
+        {[...new Array(currentBiographyParagraphs)].map((_, index) => (
+          <li key={index}>- {t(`biography-${index + 1}`)}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
