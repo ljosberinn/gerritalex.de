c9e95185c573ce0672300542e08bb8045d97152c
Gerrit Alex <galex@mpunkt.com>
2019-09-11 12:02:13 +0200

    useEffect'ed the updater

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './assets/App.scss';
 
 import {
@@ -15,34 +15,42 @@ const name = 'Gerrit Alex';
 const userName = 'ljosberinn';
 const repoLink = `https://github.com/${userName}/gerritalex.de`;
 
-(async () => {
-  const response = await fetch('//cdn.gerritalex.de/gerritalex.de/updater.php');
-  const { success, msg } = await response.json();
+const App = () => {
+  useEffect(() => {
+    const verifyDataIntegrity = async () => {
+      const response = await fetch(
+        '//cdn.gerritalex.de/gerritalex.de/updater.php'
+      );
+      const { success, msg } = await response.json();
 
-  if (success && msg === 'Update successful.') {
-    // eslint-disable-next-line
-    location.reload();
-  }
-})();
+      if (success && msg === 'Update successful.') {
+        // eslint-disable-next-line
+        location.reload();
+      }
+    };
 
-const App = () => (
-  <>
-    <Header repoLink={repoLink} name={name} />
-    <main className="application-main">
-      <div className="container-xl clearfix px-3 mt-4">
-        <Banner repoLink={repoLink} />
-        <Person name={name} userName={userName} />
-        <div className="col-lg-9 col-md-8 col-12 float-md-left pl-md-2">
-          <DynamicSubNav userName={userName} />
-          <div className="position-relative">
-            <CurriculumVitae />
-            <DynamicContent />
+    verifyDataIntegrity();
+  }, []);
+
+  return (
+    <>
+      <Header repoLink={repoLink} name={name} />
+      <main className="application-main">
+        <div className="container-xl clearfix px-3 mt-4">
+          <Banner repoLink={repoLink} />
+          <Person name={name} userName={userName} />
+          <div className="col-lg-9 col-md-8 col-12 float-md-left pl-md-2">
+            <DynamicSubNav userName={userName} />
+            <div className="position-relative">
+              <CurriculumVitae />
+              <DynamicContent />
+            </div>
           </div>
         </div>
-      </div>
-    </main>
-    <Footer />
-  </>
-);
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
