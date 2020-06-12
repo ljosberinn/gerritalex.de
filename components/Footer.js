35852e3559766ddf82a9abd2565d7c06fa585171
ljosberinn <admin@gerritalex.de>
2020-06-12 23:12:26 +0200

    fix: copyright

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,8 @@ export default function Footer() {
       <div className="position-relative d-flex flex-row-reverse flex-lg-row flex-wrap flex-lg-nowrap flex-justify-center flex-lg-justify-between pt-6 pb-2 mt-6 f6 text-gray border-top border-gray-light ">
         <ul className="list-style-none d-flex flex-wrap col-12 col-lg-5 flex-justify-center flex-lg-justify-between mb-2 mb-lg-0">
           <li className="mr-3 mr-lg-0">
-            Design © 2019 GitHub, Inc., modifications by myself
+            Design © 2019-{new Date().getFullYear()} GitHub, Inc., modifications
+            by myself
           </li>
         </ul>
       </div>
