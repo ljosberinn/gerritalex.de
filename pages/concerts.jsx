5a9ba282c02017c3bedab447158b97ad66cd2db2
Gerrit Alex <admin@gerritalex.de>
2020-09-03 01:47:34 +0200

    fix: build

--- pages/concerts.jsx
+++ pages/concerts.jsx
@@ -151,34 +151,35 @@ export default function ConcertPage({ concerts }) {
 }
 
 export function getStaticProps() {
-  concerts
-    .reduce((carry, { artist, venue, date, concert }) => {
-      // reduce to {artist: ..., shows: [showArr]}
-      const previousEntry = carry.find(dataset => dataset.artist === artist);
-
-      if (!previousEntry) {
-        return carry.concat({ artist, shows: [{ concert, date, venue }] });
-      }
-
-      return carry.map(dataset => {
-        if (dataset.artist === artist) {
-          return {
-            ...dataset,
-            shows: dataset.shows.concat({ concert, date, venue }),
-          };
-        }
-
-        return dataset;
-      });
-    }, [])
-    .sort(
-      (a, b) => (new Date(a.shows[0].date) < new Date(b.shows[0].date) ? 1 : -1) // sort DESC
-    )
-    .map(dataset => ({ ...dataset, shows: dataset.shows.reverse() })); // reverse shows
-
   return {
     props: {
-      concerts,
+      concerts: concerts
+        .reduce((carry, { artist, venue, date, concert }) => {
+          // reduce to {artist: ..., shows: [showArr]}
+          const previousEntry = carry.find(
+            dataset => dataset.artist === artist
+          );
+
+          if (!previousEntry) {
+            return carry.concat({ artist, shows: [{ concert, date, venue }] });
+          }
+
+          return carry.map(dataset => {
+            if (dataset.artist === artist) {
+              return {
+                ...dataset,
+                shows: dataset.shows.concat({ concert, date, venue }),
+              };
+            }
+
+            return dataset;
+          });
+        }, [])
+        .sort(
+          (a, b) =>
+            new Date(a.shows[0].date) < new Date(b.shows[0].date) ? 1 : -1 // sort DESC
+        )
+        .map(dataset => ({ ...dataset, shows: dataset.shows.reverse() })), // reverse shows,
     },
   };
 }
