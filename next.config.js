3b12d024e6c4ab4ce41f95357b9ed115b3dab988
Gerrit Alex <admin@gerritalex.de>
2021-01-22 15:36:48 +0100

    fix: image

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-/**
- * replaces React with Preact in prod
- * this reduces the bundle size by approx. 32 kB
- */
 const withPreact = require('next-plugin-preact');
 
 module.exports = withPreact({
-  /* regular next.js config options here */
+  images: {
+    domains: ['avatars3.githubusercontent.com'],
+  },
 });
