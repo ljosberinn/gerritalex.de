f1e37453f6c582acff5ff651220bd3fba445b534
Gerrit Alex <admin@gerritalex.de>
2025-02-05 00:23:26 +0100

    add tww s2 dispel post

--- components/WowheadLink.tsx
+++ components/WowheadLink.tsx
@@ -3,26 +3,38 @@ import { Image } from './Image';
 
 type WowheadLinkProps = {
   id: number;
-  kind: 'spell' | 'item';
+  kind: 'spell' | 'item' | 'npc' | 'achievement';
   icon: string;
   children: string;
+  iconOnly?: boolean;
 };
 
-export function WowheadLink({ id, icon, children, kind }: WowheadLinkProps) {
+export function WowheadLink({ id, icon, children, kind, iconOnly }: WowheadLinkProps) {
+  const iconJsx = icon ? (
+    <Image
+      src={`https://wow.zamimg.com/images/wow/icons/medium/${icon}.jpg`}
+      alt=""
+      width="24"
+      height="24"
+      className="mt-0 mb-0 self-center"
+    />
+  ) : null;
+
+  if (iconOnly) {
+    return (
+      <span className="inline-flex items-baseline gap-2">
+        {iconJsx}
+        {children}
+      </span>
+    );
+  }
+
   return (
     <CustomLink
       href={`https://www.wowhead.com/${kind}=${id}`}
       className="inline-flex items-baseline gap-2"
     >
-      {icon ? (
-        <Image
-          src={`https://wow.zamimg.com/images/wow/icons/medium/${icon}.jpg`}
-          alt=""
-          width="24"
-          height="24"
-          className="mt-0 mb-0 self-center"
-        />
-      ) : null}
+      {iconJsx}
       {children}
     </CustomLink>
   );
