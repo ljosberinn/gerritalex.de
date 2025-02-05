00002db93c272cee0f7609acdc925527a0ca0b44
Gerrit Alex <admin@gerritalex.de>
2025-02-05 01:19:00 +0100

    split out wowhead icon

--- components/WowheadLink.tsx
+++ components/WowheadLink.tsx
@@ -6,35 +6,23 @@ type WowheadLinkProps = {
   kind: 'spell' | 'item' | 'npc' | 'achievement';
   icon: string;
   children: string;
-  iconOnly?: boolean;
 };
 
-export function WowheadLink({ id, icon, children, kind, iconOnly }: WowheadLinkProps) {
-  const iconJsx = icon ? (
-    <Image
-      src={`https://wow.zamimg.com/images/wow/icons/medium/${icon}.jpg`}
-      alt=""
-      width="24"
-      height="24"
-      className="mt-0 mb-0 self-center"
-    />
-  ) : null;
-
-  if (iconOnly) {
-    return (
-      <span className="inline-flex items-baseline gap-2">
-        {iconJsx}
-        {children}
-      </span>
-    );
-  }
-
+export function WowheadLink({ id, icon, children, kind }: WowheadLinkProps) {
   return (
     <CustomLink
       href={`https://www.wowhead.com/${kind}=${id}`}
       className="inline-flex items-baseline gap-2"
     >
-      {iconJsx}
+      {icon ? (
+        <Image
+          src={`https://wow.zamimg.com/images/wow/icons/medium/${icon}.jpg`}
+          alt=""
+          width="24"
+          height="24"
+          className="mt-0 mb-0 self-center"
+        />
+      ) : null}
       {children}
     </CustomLink>
   );
