03bb22aacbd86b99b04af3e4b947bb63a380c80f
Gerrit Alex <admin@gerritalex.de>
2025-02-17 00:20:59 +0100

    add updated on everywhere

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -8,6 +8,7 @@ import { SectionContainer } from '../components/SectionContainer';
 import { Image } from '../components/Image';
 import { Tag } from '../components/Tag';
 import siteMetadata from '../data/siteMetadata';
+import { formatDate } from 'pliny/utils/formatDate';
 
 const editUrl = (path: string) => `${siteMetadata.siteRepo}/blob/master/data/${path}`;
 
@@ -27,7 +28,7 @@ interface LayoutProps {
 }
 
 export default function PostLayout({ content, authorDetails, next, prev, children }: LayoutProps) {
-  const { filePath, path, slug, date, title, tags } = content;
+  const { filePath, path, lastmod, date, title, tags } = content;
   const basePath = path.split('/')[0];
 
   return (
@@ -37,15 +38,34 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
+              <dl>
+                <dt className="sr-only">Published on</dt>
+                <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
+                  <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
+                </dd>
+
+                {lastmod ? (
+                  <>
+                    <dt className="sr-only">Updated on</dt>
+                    <dd className="flex items-center justify-center gap-1 text-base text-xs leading-6 font-medium text-gray-500 dark:text-gray-400">
+                      <svg
+                        width="15"
+                        height="15"
+                        viewBox="0 0 15 15"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M11.1464 6.85355C11.3417 7.04882 11.6583 7.04882 11.8536 6.85355C12.0488 6.65829 12.0488 6.34171 11.8536 6.14645L7.85355 2.14645C7.65829 1.95118 7.34171 1.95118 7.14645 2.14645L3.14645 6.14645C2.95118 6.34171 2.95118 6.65829 3.14645 6.85355C3.34171 7.04882 3.65829 7.04882 3.85355 6.85355L7.5 3.20711L11.1464 6.85355ZM11.1464 12.8536C11.3417 13.0488 11.6583 13.0488 11.8536 12.8536C12.0488 12.6583 12.0488 12.3417 11.8536 12.1464L7.85355 8.14645C7.65829 7.95118 7.34171 7.95118 7.14645 8.14645L3.14645 12.1464C2.95118 12.3417 2.95118 12.6583 3.14645 12.8536C3.34171 13.0488 3.65829 13.0488 3.85355 12.8536L7.5 9.20711L11.1464 12.8536Z"
+                          fill="currentColor"
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <time dateTime={lastmod}>{formatDate(lastmod, siteMetadata.locale)}</time>
+                    </dd>
+                  </>
+                ) : null}
               </dl>
               <div>
                 <PageTitle>{title}</PageTitle>
