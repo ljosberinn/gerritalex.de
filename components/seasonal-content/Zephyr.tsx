acc5abb8a01bcbba429cec8ee10b493fda0a16cb
Gerrit Alex <admin@gerritalex.de>
2025-02-19 00:10:58 +0100

    resurrect trash toc

--- components/seasonal-content/Zephyr.tsx
+++ components/seasonal-content/Zephyr.tsx
@@ -86,7 +86,9 @@ export function Zephyr({ data }: ZephyrProps) {
 
               return acc;
             },
-            {}
+            {
+              Trash: 'trash',
+            }
           );
 
           return (
