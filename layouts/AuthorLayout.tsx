028f217c381531788e60800b9ddee3e37f447e2a
Gerrit Alex <admin@gerritalex.de>
2025-10-17 03:41:04 +0200

    fix import

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode } from 'react';
 import { type Authors } from '../.contentlayer/generated';
-import SocialIcon from '../components/social-icons';
+import { SocialIcon } from '../components/social-icons';
 
 interface Props {
   children: ReactNode;
